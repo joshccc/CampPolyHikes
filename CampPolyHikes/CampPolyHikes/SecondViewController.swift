cc0aff655cff45a9f9ded56881ecd16f30c615ab
charnpreetsingh <csingh02@calpoly.edu>
2017-01-14 17:02:49 -0800

    add trails map

--- CampPolyHikes/CampPolyHikes/SecondViewController.swift
+++ CampPolyHikes/CampPolyHikes/SecondViewController.swift
@@ -9,38 +9,53 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var imageLabel: UILabel!
     
+    @IBOutlet var trailMap: MKMapView!
     @IBOutlet var backOutlet: UIButton!
     @IBOutlet var nextOutlet: UIButton!
     var imageInt = 0
     
+    let regionRadius: CLLocationDistance = 50
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        trailMap.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         imageInt = 1
         imageLabel.text = String("\(imageInt)/3")
+        
+        // set initial location on Terrace Hill
+        let initialLocation = CLLocation(latitude: 35.282752, longitude: -120.659615)
+        
+        centerMapOnLocation(location: initialLocation)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
     @IBAction func backButton(_ sender: Any) {
         imageInt -= 1
         imageLabel.text = String("\(imageInt)/3")
@@ -68,6 +83,6 @@ class SecondViewController: UIViewController {
             nextOutlet.isEnabled = false
             imageView.image = UIImage(named: "image3.png")
         }
-
+        
     }
 }
